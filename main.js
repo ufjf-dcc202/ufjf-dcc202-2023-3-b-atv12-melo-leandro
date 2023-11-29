cab668db797ac70f3dcbe598941a8c35a590ca60
Leandro Carlos <melo.leandro@estudante.ufjf.br>
2023-11-28 23:04:52 -0300

    acrescenta ol no codigo javascript

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const olJoao = document.querySelector("ol#joao")
+const olMaria = document.querySelector("ol#maria")
+
+
 document.entrada.addEventListener('submit', leFormulario);
 
 function leFormulario(event) {
     event.preventDefault();
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
     const destino = document.entrada.destino.value;
