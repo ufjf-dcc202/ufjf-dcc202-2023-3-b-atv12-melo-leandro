c330ace39ae64115b574105b9b96bad54925b71e
Leandro Carlos <melo.leandro@estudante.ufjf.br>
2023-11-28 22:14:02 -0300

    adiciona função para ler o formulário

---
+++ main.js
@@ -0,0 +1,10 @@
+document.entrada.addEventListener('submit', leFormulario);
+
+function leFormulario(event) {
+    event.preventDefault();
+    const quantidade = document.entrada.quantidade.value;
+    const fruta = document.entrada.fruta.value;
+
+    console.log(`Eu tenho ${quantidade} ${fruta}`);
+    // document.entrada.submit();
+}
